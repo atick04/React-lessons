452a2511186719132e8d9fa531e3fd2ef9e99fad
atick04 <aitmatov2005@gmail.com>
2022-12-07 20:19:03 +0600

    first lesson

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <input></input>
+    </div>
+  );
+}
+
+class BuyList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for{this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BuyList;
